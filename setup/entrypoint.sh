9c3244eea53f21f46a02d4776eb08b3c30ab074e
MarioWi <email@mario-wicke.de>
2021-07-16 12:57:04 +0200

    moved multicraft.key

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -233,8 +233,8 @@ if [ "$NEWINSTALL" == 1 ]; then
     if [ "$MC_KEY" != "no" ]; then
         echo
         echo "[$(date +%Y-%m-%d_%T)] - Creating Licence File"
-        echo "$MC_KEY" > "/multicraft/multicraft.key"
-        ln -s /multicraft/multicraft.key /opt/multicraft/multicraft.key
+        echo "$MC_KEY" > "/multicraft/configs/multicraft.key"
+        ln -s /multicraft/configs/multicraft.key /opt/multicraft/multicraft.key
     fi
 
     cp -r /opt/multicraft/jar/* /multicraft/jar
